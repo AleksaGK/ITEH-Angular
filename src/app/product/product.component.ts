d0df4c0a1769d5f027da8ce4732445c8d13d89fe
AleksaGK <aleksamileticgk@gmail.com>
2022-12-17 00:22:25 +0100

    drugi cas

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'app-product',
